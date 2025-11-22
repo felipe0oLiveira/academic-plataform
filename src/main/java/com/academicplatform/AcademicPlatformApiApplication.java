fe179a4292c8aad4fb56c329ace0e248e05c5f42
Felipe Oliveira <lippe637@gmail.com>
2025-11-22 10:32:34 -0300

    feat: configuraÃ§Ã£o inicial do backend Spring Boot - Academic Platform API

---
+++ src/main/java/com/academicplatform/AcademicPlatformApiApplication.java
@@ -0,0 +1,14 @@
+package com.academicplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicPlatformApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicPlatformApiApplication.class, args);
+    }
+
+}
+
